ba6b6d171cb7187b3ae1a41f8d7f649b24dee46c
Slobodan Zivanovic <172178046+slobodanzivanovic@users.noreply.github.com>
2024-07-12 19:49:31 +0200

    Support for paragraphs, bold and italic el

--- dist/example.md
+++ dist/example.md
@@ -11,3 +11,5 @@
 ###### Heading 6
 
 ########## Random heading
+
+Random text here and also **it have** bold and _italic_ inside it.
--- src/htmlize.js
+++ src/htmlize.js
@@ -36,7 +36,18 @@ class Htmlize {
         // Limit header level to h6
         headerLevel = Math.min(headerLevel, 6);
 
-        html += `<h${headerLevel}>${headerText}</h${headerLevel}>\n`;
+        html += `<h${headerLevel}>${headerText}</h${headerLevel}>\n\n`;
+      } else {
+        if (line.trim() !== "") {
+          line = line
+            // check for both bold and italic
+            // modified version of regex (https://randyperkins2k.medium.com/writing-a-simple-markdown-parser-using-javascript-1f2e9449a558)
+            .replace(/(\*\*|__)(.*?)\1/gim, "<b>$2</b>")
+            .replace(/(\*|_)(.*?)\1/gim, "<i>$2</i>");
+
+          // add to paragraph even if it is clear par or with bold/italic
+          html += `<p>${line}</p>\n\n`;
+        }
       }
     }
 
